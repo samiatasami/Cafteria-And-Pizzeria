a99d8256b50aa9c2a2fc88d478858d9f42ad9867
Samuel Damtew <samidamoljira2@gmail.com>
2025-08-19 13:32:09 +0300

    vercel added

--- caffee/src/App.jsx
+++ caffee/src/App.jsx
@@ -1,20 +1,39 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import NavBar from "./Pages/NavBar"; 
 import HomePage from "./Pages/HomePage";
 import AboutUs from "./Pages/AboutUs";
 import MenuPage from "./Pages/MenuPage";
 
+function ScrollToSection() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const hash = location.hash;
+    if (hash) {
+      const el = document.querySelector(hash);
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToSection />
       <NavBar />
-      <div className="pt-0">
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/about" element={<AboutUs/>} />
-
-          <Route path="/menu" element={<MenuPage/>} />
-        </Routes>
+      <div id="home">
+        <HomePage />
+      </div>
+      <div id="about">
+        <AboutUs />
+      </div>
+      <div id="menu">
+        <MenuPage />
       </div>
     </Router>
   );
--- caffee/src/Pages/NavBar.jsx
+++ caffee/src/Pages/NavBar.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { Menu, X } from "lucide-react"; 
+import { Menu, X } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
 
   return (
     <nav className="bg-[#fd6924fa] shadow-md fixed w-full top-0 left-0 z-50">
@@ -22,11 +23,24 @@ export default function NavBar() {
 
         {/* Desktop Menu */}
         <div className="hidden md:flex space-x-8 font-medium text-gray-700">
-          <Link to="/" className="text-white px-3 py-2 hover:border-white hover:rounded-lg hover:bg-white hover:text-[#fd6924fa] transition-all">Home</Link>
-          <Link to="/about" className="text-white px-3 py-2 hover:border-white hover:rounded-lg hover:bg-white hover:text-[#fd6924fa] transition-all">About Us</Link>
-          
-          
-          <Link to="/menu" className="text-white px-3 py-2 hover:border-white hover:rounded-lg hover:bg-white hover:text-[#fd6924fa] transition-all">Menu</Link>
+          <Link
+            to="#home"
+            className="text-white px-3 py-2 hover:border-white hover:rounded-lg hover:bg-white hover:text-[#fd6924fa] transition-all"
+          >
+            Home
+          </Link>
+          <Link
+            to="#about"
+            className="text-white px-3 py-2 hover:border-white hover:rounded-lg hover:bg-white hover:text-[#fd6924fa] transition-all"
+          >
+            About Us
+          </Link>
+          <Link
+            to="#menu"
+            className="text-white px-3 py-2 hover:border-white hover:rounded-lg hover:bg-white hover:text-[#fd6924fa] transition-all"
+          >
+            Menu
+          </Link>
         </div>
 
         {/* Mobile Hamburger */}
@@ -38,13 +52,19 @@ export default function NavBar() {
         </button>
       </div>
 
-      {/* Mobile Dropdown (floating panel) */}
+      {/* Mobile Dropdown */}
       {isOpen && (
         <div className="md:hidden absolute right-4 top-16 bg-white shadow-lg rounded-lg p-4">
           <div className="flex flex-col items-start space-y-3 font-medium">
-            <Link to="/" className="text-[#fd6924fa] hover:underline">Home</Link>
-            <Link to="/about" className="text-[#fd6924fa] hover:underline">About Us</Link>
-            <Link to="/menu" className="text-[#fd6924fa] hover:underline">Menu</Link>
+            <Link to="#home" onClick={closeMenu} className="text-[#fd6924fa] hover:underline">
+              Home
+            </Link>
+            <Link to="#about" onClick={closeMenu} className="text-[#fd6924fa] hover:underline">
+              About Us
+            </Link>
+            <Link to="#menu" onClick={closeMenu} className="text-[#fd6924fa] hover:underline">
+              Menu
+            </Link>
           </div>
         </div>
       )}
